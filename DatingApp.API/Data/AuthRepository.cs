51f3dfa55f264cecc6311bd99095e8c745d62275
Marco <marco.mastio@gmail.com>
2019-04-07 13:15:42 +0200

    added authetication tu AuthController

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -12,13 +13,14 @@ namespace DatingApp.API.Data
             _context = context;
 
         }
+        
         public async Task<User> Register(User user, string password)
         {
             byte[] passwordHash, passwordSalt;
             CreatePasswordHash(password, out passwordHash, out passwordSalt);
 
             user.PasswordHash = passwordHash;
-            user.PasswordHash = passwordSalt;
+            user.PasswordSalt = passwordSalt;
 
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
@@ -28,7 +30,7 @@ namespace DatingApp.API.Data
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 //HMACSHA512 gives a randomly generated key, that i can use as my password salt
                 passwordSalt = hmac.Key;
@@ -36,14 +38,44 @@ namespace DatingApp.API.Data
             }
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            User user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+
+            //the controller will convert this in a 401 http error
+            if (user == null)
+                return null;
+
+            //same as above
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            return user;
+
         }
 
-        public Task<bool> UserExists(string username)
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            throw new System.NotImplementedException();
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    //if any byte is not equal, the password doesnt match
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await (_context.Users.AnyAsync(x => x.Username  == username));
+            
         }
     }
 }
